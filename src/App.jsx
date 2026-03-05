7218b994ce6db3fc5f6a7fadcbd93473d74e2f96
Leonardo Cruz <kiddvverde@MacBook-Air-de-Leonardo.local>
2026-03-05 09:58:01 -0600

    Style minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import Navbar from "./components/Navbar"
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-linear-to-b from-green-500 to-green-900">
-        <div className="max-w-2xl mx-auto bg-black min-h-screen shadow-xl/100">
+      <div className="min-h-screen bg-black">
+        <div className="max-w-2xl mx-auto bg-black border-5 border-zinc-800 min-h-screen shadow-xl/100">
           <Navbar />
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 function Navbar() {
     return(
-        <nav className="flex justify-between px-4 py-3">
+        <nav className="flex justify-between px-4 py-3 border-b-5 border-zinc-800">
             <button className="text-white bg-neutral-primary border-green-600">ES</button>
             <button className="text-black bg-white">Modo nocturno</button>
         </nav>
