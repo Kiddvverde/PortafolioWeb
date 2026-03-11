50ec62791460b3ef9b5d75fee0bfdbd24c3a2dad
Kiddvverde <leonardo.mainkra@gmail.com>
2026-03-11 01:51:38 -0600

    Add basic info and minor style changes

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,7 +1,12 @@
 function ProfileCard() {
     return (
-        <div className="w-160 h-60 bg-black border-5 border-zinc-800 mx-auto mt-6">
-            <img className="w-50 h-50 m-4" src="/leomacbookcamera.jpeg" alt="Leonardo" />
+        <div className="flex items-start w-max w-max-xl h-max bg-black border-5 border-dashed border-zinc-800 mx-auto mt-6">
+            <img className="w-60 h-50 m-4 object-cover border-5 border-zinc-800" src="/leomacbookcamera.jpeg" alt="Leonardo"/>
+            <div className="flex flex-col m-3">
+                <h1 className="text-white font-pixel text-2xl">Leonardo Cruz</h1>
+                <p className="text-white font-pixel">Fullstack Web Developer specialized in the PERN Stack</p>
+                <p className="text-white font-pixel">Based in Monterrey, México 🇲🇽</p>
+            </div>
         </div>
     )
 }
